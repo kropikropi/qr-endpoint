26933b5b5726616d0ee1ca85c42da7f42aef4321
kropikropi <57503385+kropikropi@users.noreply.github.com>
2026-02-16 20:57:04 +0100

    Create qr.js

---
+++ api/qr.js
@@ -0,0 +1,28 @@
+export default function handler(req, res) {
+
+  const bic  = "AIRACZPP";
+  const name = "TaubenAbwehrSpikes.at";
+  const iban = "CZ4930300000003496321046";
+
+  let amount = req.query.amount || "0";
+  let vs     = req.query.vs || "";
+
+  amount = amount.replace(",", ".");
+  amount = parseFloat(amount).toFixed(2);
+
+  const epc =
+`BCD
+002
+1
+SCT
+${bic}
+${name}
+${iban}
+EUR${amount}
+
+${vs}`;
+
+  const encoded = encodeURIComponent(epc);
+
+  res.redirect(`https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encoded}`);
+}
